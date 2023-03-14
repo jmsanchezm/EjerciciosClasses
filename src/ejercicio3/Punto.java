7d5f316fabda2c510f85cbcfd2eed9ca5931ab6b
jmmoreno <jmmoreno@106-26.info2>
2023-03-14 11:12:10 +0100

    Coordernadas X Y

---
+++ src/ejercicio3/Punto.java
@@ -0,0 +1,39 @@
+package ejercicio3;
+
+public class Punto {
+	/**
+	 * Se guardará la coordenada del eje x
+	 */ 
+	int x;
+	/**
+	 * Se guardará la coordenada del eje y
+	 */
+	int y; 
+	
+	/**
+	 * Constructor con parámetros
+	 * @param x
+	 * @param y
+	 */
+	public Punto(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	/**
+	 * Método que imprime las coordenadas de un punto
+	 * @return Devuelve el valor de imprime
+	 */
+	public String toString () {
+		String imprime = "(" + x + ", "+ y+")";
+		return imprime;
+	}
+	/**
+	 * Método que modifica las coordenadas
+	 * @param x
+	 * @param y
+	 */
+	public void SetXY (int x, int y ) {
+		
+	}
+}
